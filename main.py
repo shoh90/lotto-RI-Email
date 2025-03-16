9d46cc2772db15fe63d359aa3d8ba9feb8b7bd74
shoh90 <52881023+shoh90@users.noreply.github.com>
2025-03-16 19:27:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from lotto import select_numbers, env, model
+from lotto import select_numbers, env
 from email_service import send_email
 
 # 로또 번호 추천 및 이메일 전송
-recommended_numbers = select_numbers(model, env)
+recommended_numbers = select_numbers(env)
+print(f"추천 로또 번호: {recommended_numbers}")
+
 send_email(recommended_numbers)
