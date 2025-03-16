754cc2fb187cc8e54ea2ef3824697f25a07955c3
shoh90 <52881023+shoh90@users.noreply.github.com>
2025-03-16 19:37:17 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,37 +1,37 @@
-numpy를 np로 가져오기
-tensorflow를 tf로 가져오기
-텐서플로우에서.keras.models 가져오기 순차
-텐서플로우에서.keras.layers가 Dense 가져오기
-텐서플로우에서.keras.optimizer가 Adam을 가져옵니다
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 
-클래스 로또DQN:
- def __init__(자기, n_numbers=45, n_select=6):
- self.n_numbers = n_numbers
- self.n_select = n_select
- 자아.모델 = 자아.빌드_모델 ()
+class LottoDQN:
+    def __init__(self, n_numbers=45, n_select=6):
+        self.n_numbers = n_numbers
+        self.n_select = n_select
+        self.model = self.build_model()
     
- def build_model(자체):
- 모델 = 순차([
- 밀집(128, 활성화='relu', input_shape=(self.n_numbers,)),
- 밀집(128, 활성화='relu'),
- 조밀함(자기 자신n_n움버, 활성화='선형')
+    def build_model(self):
+        model = Sequential([
+            Dense(128, activation='relu', input_shape=(self.n_numbers,)),
+            Dense(128, activation='relu'),
+            Dense(self.n_numbers, activation='linear')
         ])
- model.compile(loss='mse', 최적화자=Adam(learning_rate=0.001)
- 반환 모델
+        model.compile(loss='mse', optimizer=Adam(learning_rate=0.001))
+        return model
     
- def train(자기, 로또_results, 에포크=1000):
- X_train = np.array([np.bincount(numbers, 최소 길이=self)].로또_results]의 숫자에 대한 n_numbers)
- Y_train = X_train # DQN을 위한 타겟 값 설정 (당첨 패턴 학습)
+    def train(self, lotto_results, epochs=1000):
+        X_train = np.array([np.bincount(numbers, minlength=self.n_numbers) for numbers in lotto_results])
+        Y_train = X_train  # DQN을 위한 타겟 값 설정 (당첨 패턴 학습)
         
- 자기 모델.적합(X_train, Y_train, 에포크=epoch, 장황=1)
+        self.model.fit(X_train, Y_train, epochs=epochs, verbose=1)
     
- def predict_numbers(자체):
- 상태 = np.zeroos((1, self).n_numbers)
- q_values = self.model.predict(상태)
- np.argsort(q_values[0])[-self]를 반환합니다.n_select:]
+    def predict_numbers(self):
+        state = np.zeros((1, self.n_numbers))
+        q_values = self.model.predict(state)
+        return np.argsort(q_values[0])[-self.n_select:]
     
- def save_model(자체, 파일 이름="lotto_dqn.h5"):
- self.model.save(filename)
+    def save_model(self, filename="lotto_dqn.h5"):
+        self.model.save(filename)
     
- def load_model(자체, 파일 이름="lotto_dqn.h5"):
- self.model = tf.keras.models.load_model(filename)
+    def load_model(self, filename="lotto_dqn.h5"):
+        self.model = tf.keras.models.load_model(filename)
