5b50447a334e9ac212c04e5e3a0ad3130346a91b
shoh90 <52881023+shoh90@users.noreply.github.com>
2025-03-16 19:35:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from lotto import select_numbers, env
+from lotto import optimized_numbers
 from email_service import send_email
 
-# 로또 번호 추천 및 이메일 전송
-recommended_numbers = select_numbers(env)
-print(f"추천 로또 번호: {recommended_numbers}")
-
-send_email(recommended_numbers)
+# 추천된 로또 번호 출력 및 이메일 전송
+print(f"최종 추천 로또 번호: {optimized_numbers}")
+send_email(optimized_numbers)
