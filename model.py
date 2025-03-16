9841bfa62b46abc87c83b58290e73fecfc094993
shoh90 <52881023+shoh90@users.noreply.github.com>
2025-03-16 19:33:37 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,37 @@
-from sklearn.neural_network import MLPRegressor
+numpy를 np로 가져오기
+tensorflow를 tf로 가져오기
+텐서플로우에서.keras.models 가져오기 순차
+텐서플로우에서.keras.layers가 Dense 가져오기
+텐서플로우에서.keras.optimizer가 Adam을 가져옵니다
 
-def build_model(input_dim):
-    model = MLPRegressor(hidden_layer_sizes=(64, 64), activation='relu', max_iter=500)
-    return model
+클래스 로또DQN:
+ def __init__(자기, n_numbers=45, n_select=6):
+ self.n_numbers = n_numbers
+ self.n_select = n_select
+ 자아.모델 = 자아.빌드_모델 ()
+    
+ def build_model(자체):
+ 모델 = 순차([
+ 밀집(128, 활성화='relu', input_shape=(self.n_numbers,)),
+ 밀집(128, 활성화='relu'),
+ 조밀함(자기 자신n_n움버, 활성화='선형')
+        ])
+ model.compile(loss='mse', 최적화자=Adam(learning_rate=0.001)
+ 반환 모델
+    
+ def train(자기, 로또_results, 에포크=1000):
+ X_train = np.array([np.bincount(numbers, 최소 길이=self)].로또_results]의 숫자에 대한 n_numbers)
+ Y_train = X_train # DQN을 위한 타겟 값 설정 (당첨 패턴 학습)
+        
+ 자기 모델.적합(X_train, Y_train, 에포크=epoch, 장황=1)
+    
+ def predict_numbers(자체):
+ 상태 = np.zeroos((1, self).n_numbers)
+ q_values = self.model.predict(상태)
+ np.argsort(q_values[0])[-self]를 반환합니다.n_select:]
+    
+ def save_model(자체, 파일 이름="lotto_dqn.h5"):
+ self.model.save(filename)
+    
+ def load_model(자체, 파일 이름="lotto_dqn.h5"):
+ self.model = tf.keras.models.load_model(filename)
